7dc1fc3bd8340021ac37dafabca1990fbf2f0112
Nick Talbot <solutions@magichat.biz>
2017-03-31 16:38:03 +0100

    Small refactor explaining intent when starting actors

--- src/main/scala/gov/uk/justice/digital/pollpush/Server.scala
+++ src/main/scala/gov/uk/justice/digital/pollpush/Server.scala
@@ -1,10 +1,11 @@
 package gov.uk.justice.digital.pollpush
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{Actor, ActorSystem, Props}
 import com.google.inject.{Guice, Module}
 import gov.uk.justice.digital.pollpush.actor.{Poller, Pusher}
 import grizzled.slf4j.Logging
 import net.codingwell.scalaguice.InjectorExtensions._
+import scala.reflect.ClassTag
 
 object Server extends App with Logging {
 
@@ -15,8 +16,11 @@ object Server extends App with Logging {
     val injector = Guice.createInjector(config)
     val system = injector.instance[ActorSystem]
 
-    system.actorOf(Props(injector.instance[Pusher]), "Pusher")
-    system.actorOf(Props(injector.instance[Poller]), "Poller")
+    def startActor[T <: Actor: Manifest] =
+      system.actorOf(Props(injector.instance[T]), implicitly[ClassTag[T]].runtimeClass.getName.split('.').last)
+
+    startActor[Pusher]
+    startActor[Poller]
 
     system
   }
