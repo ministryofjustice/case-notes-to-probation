4ebe95aea4142673a37e16cbd451bbaa9b9fdcbd
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-01-22 10:34:11 +0000

    DT-493 Debug failing tests on circle

--- src/test/kotlin/uk/gov/justice/digital/hmpps/pollpush/config/TestQueueConfig.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/pollpush/config/TestQueueConfig.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.pollpush.config
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.model.CreateQueueRequest
 import com.amazonaws.services.sqs.model.QueueAttributeName
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.context.annotation.Bean
@@ -13,10 +15,17 @@ import org.springframework.context.annotation.Configuration
 open class TestQueueConfig(
     private val awsSqsClient: AmazonSQS,
     @Value("\${sqs.queue.name}") private val queueName: String,
-    @Value("\${sqs.dlq.name}") private val dlqName: String) {
+    @Value("\${sqs.dlq.name}") private val dlqName: String,
+    @Value("\${sqs.provider}") private val provider: String,
+    @Value("\${sqs.embedded}") private val embedded: String) {
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
 
   @Bean
   open fun queueUrl(): String {
+    log.info("sqs.provider=${provider}, embedded=${embedded}")
     val result = awsSqsClient.createQueue(CreateQueueRequest(dlqName))
     val dlqArn = awsSqsClient.getQueueAttributes(result.queueUrl, listOf(QueueAttributeName.QueueArn.toString()))
     awsSqsClient.createQueue(CreateQueueRequest(queueName).withAttributes(
