bb759405920c3bd2f98c14e46b017805c34583e9
petergphillips <peter@greenthistle.co.uk>
2020-07-28 12:13:35 +0100

    DT-1040: ⬆️ Upgrade spring boot (#51)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.7"
   kotlin("plugin.spring") version "1.3.72"
 }
 
@@ -29,12 +29,12 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
   implementation("org.springframework:spring-jms")
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.804"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.828"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
 
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.github.tomakehurst:wiremock-standalone:2.26.3")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.18.0")
+  testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.1")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.18.1")
   testImplementation("org.testcontainers:localstack:1.14.3")
   testImplementation("org.testcontainers:junit-jupiter:1.14.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
