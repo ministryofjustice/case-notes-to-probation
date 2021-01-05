6b2ae25a106434b45e66d451385c71eb5d5b1264
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-01-05 10:50:07 +0000

    DT-1437 Latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.2"
   kotlin("plugin.spring") version "1.4.21"
 }
 
@@ -30,7 +30,7 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
   implementation("org.springframework:spring-jms")
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.918"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.931"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
 
   testImplementation("org.springframework.security:spring-security-test")
