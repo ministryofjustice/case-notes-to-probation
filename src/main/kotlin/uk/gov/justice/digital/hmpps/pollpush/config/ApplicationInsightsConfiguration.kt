76fe5bc20bc5a9bcdd531aebfa3f476bdf3537d0
Peter Phillips <peter@greenthistle.co.uk>
2021-01-26 11:04:49 +0000

    DT-1541: ⬆️ Fix package name

--- src/main/kotlin/uk/gov/justice/digital/hmpps/pollpush/config/ApplicationInsightsConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/pollpush/config/ApplicationInsightsConfiguration.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.hmpps.casenotes.config
+package uk.gov.justice.digital.hmpps.pollpush.config
 
 import com.microsoft.applicationinsights.TelemetryClient
 import org.slf4j.LoggerFactory
