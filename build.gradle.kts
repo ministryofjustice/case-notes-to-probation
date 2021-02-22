1cfb53063fd8cc140e7f1636af8f5649403a7cc5
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-02-22 14:46:54 +0000

    DT-1638 useLatestVersions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
   kotlin("plugin.spring") version "1.4.30"
 }
 
@@ -18,25 +18,25 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.2")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.2")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.2")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.4")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("com.sun.xml.bind:jaxb-impl:2.3.3")
-  implementation("com.sun.xml.bind:jaxb-core:2.3.0.1")
+  implementation("com.sun.xml.bind:jaxb-impl:3.0.0")
+  implementation("com.sun.xml.bind:jaxb-core:3.0.0")
 
   implementation("com.google.code.gson:gson:2.8.6")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
   implementation("org.springframework:spring-jms")
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.942"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.959"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.22.1")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.24.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
 }
