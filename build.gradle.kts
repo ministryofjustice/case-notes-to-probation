030c13e274850f9b8777589a618158607a5d2561
Peter Phillips <peter@greenthistle.co.uk>
2021-05-14 16:15:55 +0100

    DT-2023: ⬆️  Upgrade to app insights 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.1"
   kotlin("plugin.spring") version "1.5.0"
 }
 
@@ -18,24 +18,24 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("com.sun.xml.bind:jaxb-impl:3.0.0")
-  implementation("com.sun.xml.bind:jaxb-core:3.0.0")
+  implementation("com.sun.xml.bind:jaxb-impl:3.0.1")
+  implementation("com.sun.xml.bind:jaxb-core:3.0.1")
 
   implementation("com.google.code.gson:gson:2.8.6")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
   implementation("org.springframework:spring-jms")
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.989"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.1018"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
 
-  testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
+  testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
